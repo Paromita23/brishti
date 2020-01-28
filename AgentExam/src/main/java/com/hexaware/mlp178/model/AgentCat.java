35ef8e5eb6e461a23d98d9395bd60e558519a534
Paromita <paromita2398@gmail.com>
2020-01-28 11:16:02 +0530

    exam

---
+++ AgentExam/src/main/java/com/hexaware/mlp178/model/AgentCat.java
@@ -0,0 +1,15 @@
+package com.hexaware.mlp178.model;
+
+/**
+ * @author paromita
+ */
+public enum AgentCat {
+    /**
+     * Male.
+     */
+    MALE,
+    /**
+     * female.
+     */
+    FEMALE
+}
